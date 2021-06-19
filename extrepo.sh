187025a388c6d1e0cfa1efbca72d6dca51ae71ae
ANGO <8509795+justango@users.noreply.github.com>
2021-06-19 12:03:13 +0800

    Create extrepo.sh
    Add Source Code

---
+++ extrepo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Copyright (C) 2021 Ango <m@ango.me>
+
+cd ./openwrt/package
+
+# Add Source Code
+# git clone https://git.example.com
+git clone https://github.com/cokebar/openwrt-vlmcsd
+git clone https://github.com/cokebar/luci-app-vlmcsd
+git clone https://github.com/MACJiaoLuo/luci-app-vsftpd
