829d0b0259d1a35b81792d4467ed00459c45bcff
ANGO <8509795+justango@users.noreply.github.com>
2021-06-20 00:09:26 +0800

    Update extrepo.sh

--- extrepo.sh
+++ extrepo.sh
@@ -5,5 +5,5 @@ cd ./openwrt/package
 
 # Add Source Code
 # git clone https://git.example.com
-git clone https://github.com/cokebar/openwrt-vlmcsd
-git clone https://github.com/cokebar/luci-app-vlmcsd
+# git clone https://github.com/cokebar/openwrt-vlmcsd
+# git clone https://github.com/cokebar/luci-app-vlmcsd
