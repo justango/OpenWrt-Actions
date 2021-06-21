19d973b6f4cf8896074560fd7988485a50cfaf3a
ANGO <8509795+justango@users.noreply.github.com>
2021-06-21 23:06:14 +0800

    Update extrepo.sh

--- extrepo.sh
+++ extrepo.sh
@@ -5,4 +5,5 @@ cd ./openwrt/package
 
 # Add Source Code
 # git clone https://git.example.com
-git clone https://github.com/vernesong/OpenClash
+git clone https://github.com/kenzok8/small
+git clone https://github.com/kenzok8/openwrt-packages
